a7a65629f839e8db05f6a2f58b2b20302b7723ef
Jera <jjera2617@gmail.com>
2024-02-06 14:47:19 -0500

    commit

--- src/models.js
+++ src/models.js
@@ -1,11 +1,9 @@
 const models = [
     { id: 1, nombre: "Anillo Diamante Circular" , front: 'assets/F_Ring_Diamonds1.png' , back: 'assets/B_Ring_Diamonds1.png' },
-    { id: 2, nombre: "Anillo Zafiro Oro" , front: "assets/F_Ring_Diamonds2.png" , back: "assets/B_Ring_Diamonds2.png" },
+    { id: 2, nombre: "Anillo Zafiro" , front: "assets/F_Ring_Diamonds2.png" , back: "assets/B_Ring_Diamonds2.png" },
     { id: 3, nombre: "Anillo Diamante Cuadrado" , front: "assets/F_Ring_Diamonds3.png" , back: "assets/B_Ring_Diamonds3.png" },
     { id: 4, nombre: "Anillo Zafiro Circular" , front: 'assets/F_Ring_Diamonds4.png' , back: 'assets/B_Ring_Diamonds4.png' },
     { id: 5, nombre: "Anillo Diamante Oro" , front: "assets/F_Ring_Diamonds5.png" , back: "assets/B_Ring_Diamonds5.png" },
     { id: 6, nombre: "Anillo Diamante Oro Rosa" , front: "assets/F_Ring_Diamonds6.png" , back: "assets/B_Ring_Diamonds6.png" }
   ];
-  
   export {models}
-
