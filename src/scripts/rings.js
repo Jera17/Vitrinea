36287a1cf65a6c17772d996abf3a40384a060a95
Jera <jjera2617@gmail.com>
2024-03-06 09:42:01 -0500

    Old Hand/Rings simulator replaced with the New one

--- src/scripts/rings.js
+++ src/scripts/rings.js
@@ -1,134 +1,36 @@
-// Import Objects from other .Js
-import { gestures } from "./rings_gestures.js"
+const video = document.getElementsByClassName('input_video')[0];
+const canvas = document.querySelector("#pose-canvas")
+const ctx = canvas.getContext("2d")
 import { models } from "./rings_models.js"
-//Cam Config
-const x = 400
-const y = x/(16/9)
-console.log(x)
-const config = { video: { width: x, height: y, fps: 30 } }
-//Initialize variables just ONE time when code start
-var idModel;
-if (!idModel) {
-  var idModel = 0
-  var fingerIndex = 1 // 0 = Index, 1 = Middel, 2 = Ring, 3 = Pinky
-  var condicional = true
-}
-//Identify gestures 
-const gestureStrings = {
-  'rock': '✊️',
-  'paper': '🖐',
-  'scissors': '✌️'
-}
-//Create Hand Detector
-async function createDetector() {
-  return window.handPoseDetection.createDetector(
-    window.handPoseDetection.SupportedModels.MediaPipeHands,
-    {
-      runtime: "mediapipe",
-      modelType: "full",
-      maxHands: 1,
-      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.4.1646424915`
-    }
-  )
-}
+var idModel = 0
 
-async function main() {
-  const video = document.getElementsByClassName('input_video')[0];
-  var canvas = document.querySelector("#pose-canvas")
-  const ctx = canvas.getContext("2d")
-  console.log(video.width, canvas.height)
-  const resultLayer = document.querySelector("#pose-results")
-  const knownGestures = [...gestures]
-  const GE = new fp.GestureEstimator(knownGestures)
-  // load handpose model
-  const detector = await createDetector()
-  // main estimation loop
-  const estimateHands = async () => {
-    // clear canvas overlay
-    ctx.clearRect(0, 0, config.video.width, config.video.height)
-    // get hand landmarks from video
-    const hands = await detector.estimateHands(video, {
-      flipHorizontal: false
-    })
+function onResultsHands(results) {
+  //set Canva
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.save();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (const hand of hands) {
-      // console.log(hand)
-      drawImage(ctx, hand, fingerIndex)
-      const keypoints3D = hand.keypoints3D.map(keypoint => [keypoint.x, keypoint.y, keypoint.z])
-      const predictions = GE.estimate(keypoints3D, 9)
-
-      if (predictions.gestures.length > 0) {
-        const result = predictions.gestures.reduce((p, c) => (p.score > c.score) ? p : c)
-        const found = gestureStrings[result.name]
-        if (condicional) {
-          if (found === '✊️') {
-            cambiarDedo();
-          } else if (found === '✌️' && idModel < models.length - 1) {
-            updateModel(resultLayer, 1);
-          } else if (found === '✌️' && idModel === models.length - 1) {
-            updateModel(resultLayer, - (models.length - 1));
-          }
-        }
-      }
-    }
-    setTimeout(() => { estimateHands() }, 1000 / config.video.fps,)
-  }
-  estimateHands()
-}
-//Cam propieties
-async function initCamera(width, height, fps) {
-  const constraints = {
-    audio: false,
-    video: {
-      facingMode: "user",
-      width: width,
-      height: height,
-      frameRate: { max: fps }
+  if (results.multiHandLandmarks) {
+    for (let index = 0; index < results.multiHandLandmarks[0].length; index++) {
+      const isRight = (results.multiHandedness[0].label === 'Left') ? 1 : -1;
+      drawImage(results.multiHandLandmarks[0], isRight);
     }
   }
-
-  const video = document.getElementsByClassName('input_video')[0]
-  video.width = width
-  video.height = height
-  // get video stream
-  const stream = await navigator.mediaDevices.getUserMedia(constraints)
-  video.srcObject = stream
-  return new Promise(resolve => {
-    video.onloadedmetadata = () => { resolve(video) }
-  })
 }
+ctx.restore();
 
-function updateModel(resultLayer, newId) {
-  condicional = false;
-  idModel = idModel + newId;
-  setTimeout(() => { condicional = true; }, 2000);
-  resultLayer.innerText = models[idModel].nombre;
-  setTimeout(() => { resultLayer.innerText = ''; }, 3000);
-}
-
-function cambiarDedo() {
-  condicional = false
-  if (fingerIndex < 3) {
-    fingerIndex++
-  } else {
-    fingerIndex = 0
-  }
-  setTimeout(() => { condicional = true; }, 1000);
-}
-
-function drawImage(ctx, hand, fingerIndex) {
-  //Set sources Images
+function drawImage(rslt, isRight) {
   var imgFront = new Image();
   imgFront.src = models[idModel].front;
 
   var imgBack = new Image();
   imgBack.src = models[idModel].back;
-  //Chose finger
-  const fingerIdNodes = [5, 9, 13, 17]
-  const x1 = hand.keypoints[fingerIdNodes[fingerIndex]].x
-  const y1 = hand.keypoints[fingerIdNodes[fingerIndex]].y
-  const x2 = hand.keypoints[fingerIdNodes[fingerIndex] + 1].x
-  const y2 = hand.keypoints[fingerIdNodes[fingerIndex] + 1].y
+
+  const x1 = rslt[9].x * video.videoWidth
+  const y1 = rslt[9].y * video.videoHeight
+  const x2 = rslt[10].x * video.videoWidth
+  const y2 = rslt[10].y * video.videoHeight
 
   ctx.beginPath()
   ctx.save()
@@ -149,25 +51,46 @@ function drawImage(ctx, hand, fingerIndex) {
   //Scale
   var FingerLenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
 
-  //Flip
-  var HandRotationZ = 0
-  HandRotationZ = ((hand.keypoints3D[5].z + hand.keypoints3D[10].z + hand.keypoints3D[17].z + (hand.keypoints3D[0].z / 10)) / 4)
+  //Flip (Usando producto punto)
+  function crossProductFromPoints(point1A, point2A, point1B, point2B) {
+    const vectorA = [point2A[0] - point1A[0], point2A[1] - point1A[1], point2A[2] - point1A[2]];
+    const vectorB = [point2B[0] - point1B[0], point2B[1] - point1B[1], point2B[2] - point1B[2]];
+    const result = [
+      vectorA[1] * vectorB[2] - vectorA[2] * vectorB[1],
+      vectorA[2] * vectorB[0] - vectorA[0] * vectorB[2],
+      vectorA[0] * vectorB[1] - vectorA[1] * vectorB[0]
+    ];
+    return result;
+  }
 
-  const isLeftHand = hand.handedness === 'Left';
-  const selectedImage = (isLeftHand && HandRotationZ > 0) || (!isLeftHand && HandRotationZ > 0) ? imgFront : imgBack
+  // Ejemplo de uso:
+  const point1A = [rslt[9].x, rslt[9].y, 0];
+  const point2A = [rslt[10].x, rslt[10].y, 0];
+  const point1B = [rslt[9].x, rslt[9].y, 0];
+  const point2B = [rslt[13].x, rslt[13].y, 0];
+
+  const result = crossProductFromPoints(point1A, point2A, point1B, point2B);
+
+  const selectedImage = ((result[2] * isRight) > 0) ? imgFront : imgBack
   ctx.drawImage(selectedImage, (0 - FingerLenght / 4), (0 - FingerLenght / 2) / 1.25, FingerLenght / 2, FingerLenght / 2)
 
   ctx.restore()
   ctx.closePath()
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  initCamera(
-    config.video.width, config.video.height, config.video.fps
-  ).then(video => {
-    video.play()
-    video.addEventListener("loadeddata", event => {
-      main()
-    })
-  })
-})
+
+
+
+const hands = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
+  }
+});
+hands.onResults(onResultsHands);
+
+const camera = new Camera(video, {
+  onFrame: async () => { await hands.send({ image: video }); },
+  width: canvas.width,
+  height: canvas.height
+});
+camera.start();
