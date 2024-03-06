b8f450e6176a0d25d0cd40e83a92e2cda0043588
Jera <jjera2617@gmail.com>
2024-03-06 09:39:47 -0500

    New Hand Structure Completed

--- src/scripts/hands.js
+++ src/scripts/hands.js
@@ -3,8 +3,6 @@ const canvas = document.querySelector("#pose-canvas")
 const ctx = canvas.getContext("2d")
 import { models } from "./rings_models.js"
 var idModel = 0
-var glasses = new Image();
-glasses.src = models[idModel].img
 
 function onResultsHands(results) {
   //set Canva
@@ -33,12 +31,6 @@ function drawImage(rslt, isRight) {
   const y1 = rslt[9].y * video.videoHeight
   const x2 = rslt[10].x * video.videoWidth
   const y2 = rslt[10].y * video.videoHeight
-  //draw circles
-  ctx.beginPath();
-  ctx.arc(x1, y1, 3, 0, 2 * Math.PI);
-  ctx.arc(x2, y2, 3, 0, 2 * Math.PI);
-  ctx.fillStyle = 'blue';
-  ctx.fill();
 
   ctx.beginPath()
   ctx.save()
@@ -59,51 +51,27 @@ function drawImage(rslt, isRight) {
   //Scale
   var FingerLenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
 
-  //Flip
-  var HandRotationZ = 0
-  HandRotationZ = ((rslt[5].z + rslt[10].z + rslt[17].z + (rslt[0].z / 10)) / 4)
-  const selectedImage = (isRight && HandRotationZ > 0) || (!isRight && HandRotationZ > 0) ? imgFront : imgBack
-
-
-  //Producto punto
-  //Vectores
+  //Flip (Usando producto punto)
   function crossProductFromPoints(point1A, point2A, point1B, point2B) {
     const vectorA = [point2A[0] - point1A[0], point2A[1] - point1A[1], point2A[2] - point1A[2]];
     const vectorB = [point2B[0] - point1B[0], point2B[1] - point1B[1], point2B[2] - point1B[2]];
-
     const result = [
-        vectorA[1] * vectorB[2] - vectorA[2] * vectorB[1],
-        vectorA[2] * vectorB[0] - vectorA[0] * vectorB[2],
-        vectorA[0] * vectorB[1] - vectorA[1] * vectorB[0]
+      vectorA[1] * vectorB[2] - vectorA[2] * vectorB[1],
+      vectorA[2] * vectorB[0] - vectorA[0] * vectorB[2],
+      vectorA[0] * vectorB[1] - vectorA[1] * vectorB[0]
     ];
-
     return result;
-}
+  }
 
-// Ejemplo de uso:
-const point1A = [rslt[9].x, rslt[9].y, 0];
-const point2A = [rslt[10].x, rslt[10].y, 0];
-const point1B = [rslt[9].x, rslt[9].y, 0];
-const point2B = [rslt[13].x, rslt[13].y, 0];
+  // Ejemplo de uso:
+  const point1A = [rslt[9].x, rslt[9].y, 0];
+  const point2A = [rslt[10].x, rslt[10].y, 0];
+  const point1B = [rslt[9].x, rslt[9].y, 0];
+  const point2B = [rslt[13].x, rslt[13].y, 0];
 
-const result = crossProductFromPoints(point1A, point2A, point1B, point2B);
+  const result = crossProductFromPoints(point1A, point2A, point1B, point2B);
 
-//ARREGLAR QUE RECONOZCA QUE MANO ES CUAL Y SEGUN ESO ES POSITIVO O NEGATIVO
-// console.log(result[2], isRight)
-
-if ((result[2]*isRight) < 0) {
-  console.log('palma');
-} else {
-  console.log('dorso');
-}
-
-
-  ctx.beginPath();
-  ctx.arc(0, 0, 3, 0, 2 * Math.PI);
-  ctx.fillStyle = 'blue';
-  ctx.fill();
-
-
+  const selectedImage = ((result[2] * isRight) > 0) ? imgFront : imgBack
   ctx.drawImage(selectedImage, (0 - FingerLenght / 4), (0 - FingerLenght / 2) / 1.25, FingerLenght / 2, FingerLenght / 2)
 
   ctx.restore()
