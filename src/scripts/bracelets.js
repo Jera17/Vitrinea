6780abe0a14164499b78d483ca02928fa0356d9c
Jera <jjera2617@gmail.com>
2024-03-15 15:24:37 -0500

    Bracalets simulator done

---
+++ src/scripts/bracelets.js
@@ -0,0 +1,163 @@
+const video = document.getElementsByClassName('input_video')[0];
+const canvas = document.querySelector("#pose-canvas")
+const ctx = canvas.getContext("2d")
+const buttons = document.querySelectorAll(".my-button");
+
+import { models } from "./bracelets_models.js"
+var idModel = 0
+var imgFront = new Image();
+imgFront.src = models[idModel].front;
+var imgBack = new Image();
+imgBack.src = models[idModel].back;
+
+const manualAjust = 10
+var translationDistance = 5
+var upAndDown = 0
+var newYposition = 0
+var leftAndRight = 0
+var newXposition = 0
+var zoomInAndOut = 0
+var newScale = 1
+
+function onResultsHands(results) {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (results.multiHandLandmarks) {
+    const isRight = (results.multiHandedness[0].label === 'Left') ? 1 : -1;
+    drawImage(results.multiHandLandmarks[0], isRight);
+  }
+}
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    switch (button.id) {
+      case "Up":
+      case "Down":
+        updateY(button.id);
+        break;
+      case "Left":
+      case "Right":
+        updateX(button.id);
+        break;
+      case "ZoomIn":
+      case "ZoomOut":
+        updateZoom(button.id);
+        break;
+      case "ChangeLeft":
+      case "ChangeRight":
+        updateCounter(button.id);
+        break;
+      case "ChangeFingerLeft":
+      case "ChangeFingerRight":
+        updateFinger(button.id);
+        break;
+      default:
+        console.log("Unknown button clicked");
+    }
+  });
+});
+
+function updateY(buttonId) {
+  if (buttonId === "Up" && upAndDown < manualAjust) {
+    upAndDown++;
+  } else if (buttonId === "Down" && upAndDown > -manualAjust) {
+    upAndDown--;
+  }
+  newYposition = upAndDown * translationDistance;
+}
+
+function updateX(buttonId) {
+  if (buttonId === "Left" && leftAndRight < manualAjust) {
+    leftAndRight++;
+  } else if (buttonId === "Right" && leftAndRight > -manualAjust) {
+    leftAndRight--;
+  }
+  newXposition = leftAndRight * translationDistance;
+}
+
+function updateZoom(direction) {
+  const delta = (direction === "ZoomIn") ? 1 : -1;
+  if (zoomInAndOut + delta >= -manualAjust && zoomInAndOut + delta <= manualAjust) {
+    zoomInAndOut += delta;
+    newScale = 1 + (zoomInAndOut * 0.05);
+  }
+}
+
+function updateCounter(operator) {
+  idModel = (operator === 'ChangeRight') ? (idModel + 1) % models.length : (idModel - 1 + 3) % models.length;
+  imgFront.src = models[idModel].front;
+  imgBack.src = models[idModel].back;
+}
+
+function drawImage(rslt, isRight) {
+  ctx.save();
+  const x1 = rslt[0].x * video.videoWidth
+  const y1 = rslt[0].y * video.videoHeight
+  const x2 = rslt[9].x * video.videoWidth
+  const y2 = rslt[9].y * video.videoHeight
+
+  ctx.beginPath()
+  ctx.save()
+  //set the position center of the canva and from hand
+  const tanx = (x1 - x2)
+  const tany = (y1 - y2)
+  const pstx = x1 + tanx
+  const psty = y1 + tany
+  console.log(tanx, tany)
+  ctx.translate(pstx, psty)
+
+  //set angle of the image
+  var componenteX = 1
+  if ((x1 - x2) > 0) {
+    componenteX = -1
+  }
+  const pendiente = ((y2 - y1) / (x2 - x1))
+  const angleHand = Math.atan(pendiente)
+  ctx.rotate(angleHand + ((Math.PI / 2))*componenteX)
+
+  //Scale
+  // var FingerLenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) * 2 * newScale
+  var FingerLenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) * 2 * newScale
+
+  //Flip (Usando producto punto)
+  function crossProductFromPoints(point1A, point2A, point1B, point2B) {
+    const vectorA = [point2A[0] - point1A[0], point2A[1] - point1A[1], point2A[2] - point1A[2]];
+    const vectorB = [point2B[0] - point1B[0], point2B[1] - point1B[1], point2B[2] - point1B[2]];
+    const result = [
+      vectorA[1] * vectorB[2] - vectorA[2] * vectorB[1],
+      vectorA[2] * vectorB[0] - vectorA[0] * vectorB[2],
+      vectorA[0] * vectorB[1] - vectorA[1] * vectorB[0]
+    ];
+    return result;
+  }
+
+  // Ejemplo de uso:
+  const point1A = [rslt[9].x, rslt[9].y, 0];
+  const point2A = [rslt[10].x, rslt[10].y, 0];
+  const point1B = [rslt[9].x, rslt[9].y, 0];
+  const point2B = [rslt[13].x, rslt[13].y, 0];
+
+  const result = crossProductFromPoints(point1A, point2A, point1B, point2B);
+
+  const selectedImage = ((result[2] * isRight) > 0) ? imgFront : imgBack
+  ctx.drawImage(selectedImage, (0 - FingerLenght / 4) + newXposition, ((0 - FingerLenght / 2) / 1.25) - newYposition, FingerLenght / 2, FingerLenght / 2)
+
+  ctx.restore()
+  ctx.closePath()
+}
+
+const hands = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
+  }
+});
+hands.onResults(onResultsHands);
+
+const camera = new Camera(video, {
+  onFrame: async () => { 
+    await hands.send({ image: video }); },
+  width: 1280,
+  height: 720
+});
+camera.start();
---
+++ src/scripts/necklaces.js
@@ -0,0 +1,116 @@
+const video = document.getElementsByClassName('input_video')[0];
+const canvas = document.querySelector("#pose-canvas")
+const ctx = canvas.getContext("2d")
+const buttons = document.querySelectorAll(".my-button");
+
+import { models } from "./clothes_models.js"
+var idModel = 0
+var clotheModel = new Image();
+clotheModel.src = models[idModel].img
+var nodes = [12, 11, 23] //Hombros-Cintura: [12, 11, 23], Hombros-Rodilla: [12, 11, 25], Hombros-Tobillo: [12, 11, 27]
+
+const manualAjust = 10
+var translationDistance = 5
+var upAndDown = 0
+var newYposition = 0
+var leftAndRight = 0
+var newXposition = 0
+
+function onResultsPose(results) {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.clearRect(0, 0, video.videoWidth, video.videoHeight)
+
+  if(results.poseLandmarks){
+    const coords = getCoords(results.poseLandmarks, nodes)
+    ctx.drawImage(clotheModel, 
+      coords.x0 - (coords.SW / 2.5) + newXposition, 
+      coords.y0 - (coords.SW / 4) - newYposition, 
+      coords.SW + (coords.SW / 1.25), 
+      coords.TH + (coords.SW / 4))
+  }
+}
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    switch (button.id) {
+      case "Up":
+      case "Down":
+        updateY(button.id);
+        break;
+      case "Left":
+      case "Right":
+        updateX(button.id);
+        break;
+      case "ChangeLeft":
+      case "ChangeRight":
+        updateCounter(button.id);
+        break;
+      default:
+        console.log("Unknown button clicked");
+    }
+  });
+});
+
+function updateY(buttonId) {
+  if (buttonId === "Up" && upAndDown < manualAjust) {
+    upAndDown++;
+  } else if (buttonId === "Down" && upAndDown > -manualAjust) {
+    upAndDown--;
+  }
+  newYposition = upAndDown * translationDistance;
+}
+
+function updateX(buttonId) {
+  if (buttonId === "Left" && leftAndRight < manualAjust) {
+    leftAndRight++;
+  } else if (buttonId === "Right" && leftAndRight > -manualAjust) {
+    leftAndRight--;
+  }
+  newXposition = leftAndRight * translationDistance;
+}
+
+function updateCounter(operator) {
+  idModel = (operator === 'ChangeRight') ? (idModel + 1) % models.length : (idModel - 1 + 3) % models.length;
+  console.log(idModel, (idModel + 1) % models.length, (idModel - 1 + models.length) % models.length)
+  clotheModel.src = models[idModel].img;
+}
+
+function getCoords(rsl, nodes) {
+  const x0 = (rsl[nodes[0]].x * video.videoWidth) //hombro izquierdo
+  const y0 = (rsl[nodes[0]].y * video.videoHeight)
+  const x1 = (rsl[nodes[1]].x * video.videoWidth) //hombro derecho
+  const y1 = (rsl[nodes[1]].y * video.videoHeight)
+  const x2 = (rsl[nodes[2]].x * video.videoWidth) //cadera derecha
+  const y2 = (rsl[nodes[2]].y * video.videoHeight)
+  const shoulderWidth = Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2)) //ancho entre hombros
+  const torsosHeight = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) //largo del hombro a la cadera
+  const magX = x1 - x0
+  const magY = y1 - y0
+  return {
+    x0: x0,
+    y0: y0,
+    x1: x1,
+    y1: y1,
+    SW: shoulderWidth,
+    TH: torsosHeight,
+    magX: magX,
+    magY: magY
+  }
+}
+
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.2/${file}`;
+  }
+});
+pose.onResults(onResultsPose);
+
+const camera = new Camera(video, {
+  onFrame: async () => {
+    await pose.send({ image: video });
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
