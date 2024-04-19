03fc97377c4c675068150d129779c0ced8ad5dd8
Jera <jjera2617@gmail.com>
2024-04-19 14:47:04 -0500

    uwu

--- src/scripts/earrings.js
+++ src/scripts/earrings.js
@@ -147,6 +147,7 @@ function imageDraw(rsl, Node1, Node2, Node3, Orientation) {
     const imageWidth = Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2)) * newScale
     const imageHeight = (image.height * imageWidth) / image.width
 
+    //uwu
     ctx.drawImage(image, AuxOrigenX - (imageWidth / 2) + (newXposition * Orientation), AuxOrigenY - (imageWidth / 2) - newYposition, imageWidth, imageHeight)
   }
 }
