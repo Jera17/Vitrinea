d22ace11d49b3b51deb5b33023fecdebb8996378
DESKTOP-N3KLSLL\HumanBionics <jjera2617@gmail.com>
2024-12-18 11:11:10 -0500

    WIP

--- src/scripts/tooth.js
+++ src/scripts/tooth.js
@@ -13,14 +13,6 @@ faceMesh.setOptions({
 
 faceMesh.onResults(onResults);
 
-function drawPoint(ctx, x, y, r, color) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.closePath()
-}
-
 // Variables
 const imageInput = document.getElementById('imageInput');
 const canvasInput = document.getElementById('canvasInput');
@@ -68,16 +60,6 @@ function onResults(results) {
   const maxY = Math.max(...mouthCoords.map(p => p.y));
   const width = maxX - minX;
   const height = maxY - minY;
-
-  // // Dibujar el rectángulo rojo alrededor de la boca
-  // ctxInput.strokeStyle = 'red';
-  // ctxInput.lineWidth = 3;
-  // ctxInput.strokeRect(minX, minY, width, height);
-
-  // // Dibujar puntos azules específicos de la boca
-  // mouthCoords.forEach(point => {
-  //   drawPoint(ctxInput, point.x, point.y, 5, 'red');
-  // });
   
   // Recortar y dibujar en el canvas de salida
   canvasOutput.width = width;
